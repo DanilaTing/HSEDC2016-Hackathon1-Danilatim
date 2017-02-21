03cb939538e77b686820db40797fed3fca224179
DanilaTing <danilatimofeev@MacBook-Air-Danila.local>
2017-02-21 03:00:41 +0300

    SVG

---
+++ javascripts/plan01.js
@@ -0,0 +1,9 @@
+$(function() {
+  $('.planLow').click(function() {
+		$('.planLow').css('animation', 'none');
+	});
+
+  $('.cls-1').click(function(){
+    $('.invisible').css('opacity', '1');
+  });
+});
--- javascripts/scroll.js
+++ javascripts/scroll.js
@@ -6,4 +6,16 @@ $(function() {
 	$('.btns p:first-of-type').click(function() {
 		$('.main').css({"margin-top": "0vh", "transition": "all 1s ease"});
 	});
+
+	$('.btns p:last-of-type').click(function() {
+		$('.main').css({"margin-top": "-200vh", "transition": "all 1s ease"});
+	});
+
+	$('.btns2 p:first-of-type').click(function() {
+		$('.main').css({"margin-top": "-100vh", "transition": "all 1s ease"});
+	});
+
+	$('.btns2 p:last-of-type').click(function() {
+		$('.main').css({"margin-top": "0vh", "transition": "all 1s ease"});
+	});
 });
