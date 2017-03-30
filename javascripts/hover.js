611fdbe210b77e7347278ef1179faf262f7d00f2
DanilaTing <danilatimofeev@MacBook-Air-Danila.local>
2017-03-31 02:20:10 +0300

    aaalmost

--- javascripts/hover.js
+++ javascripts/hover.js
@@ -16,5 +16,39 @@ $(function () {
      direction: "vertical"
   });
 
-  
+  $('#reception').click(function() {
+    $("#plan1").get(0).scrollIntoView();
+  });
+
+  $('#tests').click(function() {
+    $("#plan1").get(0).scrollIntoView();
+    $('.top_plans.first, .top_plans.third, .top_plans.fourth, .top_plans.fifth').css("display", "none");
+    $('.top_plans.second').css("display", "flex");
+  });
+
+  $('#sun').click(function() {
+    $("#plan1").get(0).scrollIntoView();
+    $('.top_plans.first, .top_plans.third, .top_plans.fourth, .top_plans.fifth').css("display", "none");
+    $('.top_plans.second').css("display", "flex");
+  });
+  $('#cloud').click(function() {
+    $("#plan1").get(0).scrollIntoView();
+    $('.top_plans.first, .top_plans.second, .top_plans.fourth, .top_plans.fifth').css("display", "none");
+    $('.top_plans.third').css("display", "flex");
+  });
+  $('#rain').click(function() {
+    $("#plan1").get(0).scrollIntoView();
+    $('.top_plans.first, .top_plans.second, .top_plans.third, .top_plans.fifth').css("display", "none");
+    $('.top_plans.fourth').css("display", "flex");
+  });
+  $('#storm').click(function() {
+    $("#plan1").get(0).scrollIntoView();
+    $('.top_plans.first, .top_plans.third, .top_plans.fourth, .top_plans.fifth').css("display", "none");
+    $('.top_plans.second').css("display", "flex");
+  });
+
+  $('#low').click(function() {
+    $(".low_plans").get(0).scrollIntoView();
+  });
+
 });
